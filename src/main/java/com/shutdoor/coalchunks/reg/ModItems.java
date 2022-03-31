abcfe44c3623e0ff696d1812ff46da317c7935c9
shutdoor <enyssa2129@gmail.com>
2022-03-31 14:24:05 -0500

    Added Fabric 1.18.2 files & zh_cn tanslation

--- src/main/java/com/shutdoor/coalchunks/reg/ModItems.java
+++ src/main/java/com/shutdoor/coalchunks/reg/ModItems.java
@@ -13,17 +13,17 @@ import net.minecraft.util.registry.Registry;
 public class ModItems{
 
     public static final Item SMALL_COAL_CHUNK = registerItem("small_coal_chunk",
-            new TranslatableText("tooltip.coalchunks.small_coal_chunk"));
+            new TranslatableText("tooltip.coalchunks.small_chunk"));
     public static final Item SMALL_CHARCOAL_CHUNK = registerItem("small_charcoal_chunk",
-            new TranslatableText("tooltip.coalchunks.small_charcoal_chunk"));
+            new TranslatableText("tooltip.coalchunks.small_chunk"));
     public static final Item MEDIUM_COAL_CHUNK = registerItem("medium_coal_chunk",
-            new TranslatableText("tooltip.coalchunks.medium_coal_chunk"));
+            new TranslatableText("tooltip.coalchunks.medium_chunk"));
     public static final Item MEDIUM_CHARCOAL_CHUNK = registerItem("medium_charcoal_chunk",
-            new TranslatableText("tooltip.coalchunks.medium_charcoal_chunk"));
+            new TranslatableText("tooltip.coalchunks.medium_chunk"));
     public static final Item LARGE_COAL_CHUNK = registerItem("large_coal_chunk",
-            new TranslatableText("tooltip.coalchunks.large_coal_chunk"));
+            new TranslatableText("tooltip.coalchunks.large_chunk"));
     public static final Item LARGE_CHARCOAL_CHUNK = registerItem("large_charcoal_chunk",
-            new TranslatableText("tooltip.coalchunks.large_charcoal_chunk"));
+            new TranslatableText("tooltip.coalchunks.large_chunk"));
 
     private static ChunkItem registerItem(String name, TranslatableText text){
         return Registry.register(Registry.ITEM, new Identifier(CoalChunks.MODID, name), new ChunkItem(text));
